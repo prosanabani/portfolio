797bb146e22ea3130bb3a8d11ff2b5e2b9a1113b
Ali Ibrahim Al-Sanabani <alsanabani971@gmail.com>
2025-01-05 20:30:12 +0300

    config: unocss

--- src/root.tsx
+++ src/root.tsx
@@ -1,38 +1,33 @@
-import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-  } from "react-router";
-  
-  export function Layout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>My App</title>
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          {children}
-          <ScrollRestoration />
-          <Scripts />
-        </body>
-      </html>
-    );
-  }
-  
-  export default function Root() {
-    return <Outlet />;
-  }
-  
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
+import 'virtual:uno.css'
+import '@unocss/reset/tailwind.css'
+
+export const Layout = ({
+  children,
+}: {
+  readonly children: React.ReactNode
+}) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf8" />
+        <meta
+          content="width=device-width, initial-scale=1.0"
+          name="viewport"
+        />
+        <title>My App</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
+
+export default function Root() {
+  return <Outlet />
+}
