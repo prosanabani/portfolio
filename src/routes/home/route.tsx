6b04f4c0268106a8ad3f6e61ee48bad1cec04a1b
Ali Ibrahim Al-Sanabani <alsanabani971@gmail.com>
2025-01-26 23:27:35 +0300

    refactor: remove unused clientLoader and clean up home route

--- src/routes/home/route.tsx
+++ src/routes/home/route.tsx
@@ -1,9 +1,3 @@
-// import { redirect } from 'react-router'
-
-// export const clientLoader = () => {
-//   return redirect('/')
-// }
-
 export default function Component() {
   return <div className="">home</div>
 }
