ded2a45a8b259b4a58774aae8e0f1901bdab9e69
Ali Ibrahim Al-Sanabani <alsanabani971@gmail.com>
2025-01-31 20:45:16 +0300

    chore: update TypeScript configuration and Vite setup to include Unimport plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import { reactRouter } from '@react-router/dev/vite'
 import UnoCSS from 'unocss/vite'
+import path from 'path'
+import Unimport from 'unimport/unplugin'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), UnoCSS()],
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    reactRouter(),
+    UnoCSS(),
+    Unimport.vite({
+      presets: ['react'],
+      dirs: [
+        './src/components/**',
+        './src/globals/**',
+        './src/middlewares/**',
+        './src/stores/**',
+        './src/queries/**',
+      ],
+      dts: true,
+    }),
+  ],
   ssr: {
     noExternal: ['primereact'],
   },
